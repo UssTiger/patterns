9be5aecf8d067b4dfbe55b81a05c744c039209ec
UssurTiger <lipinvv1986@gmail.com>
2024-10-04 14:02:56 +1000

    develop Паттерн фасад

---
+++ src/main/java/structural/facade/TestFacade.java
@@ -0,0 +1,13 @@
+package structural.facade;
+
+import java.io.File;
+
+public class TestFacade {
+
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+
+}
